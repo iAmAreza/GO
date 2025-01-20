7871dfa4e208c4ed7f7e8ff4b86b7e8db0cbba20
IAmAreza <ahmadrezacuet.cse@gmail.com>
2025-01-21 00:05:45 +0600

    update function

--- Functions/functions.go
+++ Functions/functions.go
@@ -9,11 +9,36 @@ func add(num1 int, num2 int) {
 
 }
 
+func newadd(num1 int, num2 int) int{
+    sum1 := num1 + num2 
+		return sum1 
+}
+
+
+func multiple(num1 int, num2 int) (int,int){
+	sum1 := num1 + num2
+	mul1 := num1 * num2 
+
+	return sum1, mul1  
+} 
+
+
+func printName(name string){
+	fmt.Println("This is", name) 
+}
+
 func main() {
 	a := 10
 	b := 20
 	add(a, b)
 	add(2, 3)
 	add(4, 5) 
-	
+  fmt.Println(newadd(12,12)) 
+
+	p, q := multiple(2,3) 
+
+	fmt.Println(p)
+	fmt.Println(q)
+
+  printName("Reza")
 }
