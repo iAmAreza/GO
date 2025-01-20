56cc5d4d0efa8671517c6a7562784cb433fa8f0f
IAmAreza <ahmadrezacuet.cse@gmail.com>
2025-01-20 23:36:38 +0600

    variable and conditions

---
+++ conditions/conditional.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 18
+	if age > 18 {
+		fmt.Println("you can marrid")
+	}else if (age < 18){
+		fmt.Println("you cannot married")
+	} else{
+		fmt.Println("Ja vag!")
+	}
+}
---
+++ variable-datatypes/variable_datatypes.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 20
+	fmt.Println(a) 
+
+	const b = 30 
+	fmt.Println(b) 
+
+	c := "Reza"
+	fmt.Println(c) 
+
+	
+}
