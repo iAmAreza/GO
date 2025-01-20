3deb246d0fd0c617e2bb47d5b97037e56d78abb1
IAmAreza <ahmadrezacuet.cse@gmail.com>
2025-01-20 23:35:48 +0600

    functions variable and conditions

---
+++ Functions/functions.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func add(num1 int, num2 int) {
+
+	sum := num1 + num2
+	fmt.Println(sum)
+
+}
+
+func main() {
+	a := 10
+	b := 20
+	add(a, b)
+	add(2, 3)
+	add(4, 5) 
+	
+}
